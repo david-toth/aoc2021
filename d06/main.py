e32fea5672a26f70a1e04b685b63f020c5889c48
David Toth <davidtoth99@gmail.com>
2021-12-06 09:56:20 -0500

    d06 solution

---
+++ d06/main.py
@@ -0,0 +1,24 @@
+from collections import Counter, defaultdict
+
+file = open("input.txt", "r")
+lines = file.readlines()
+file.close()
+data = list(map(int, lines[0].strip().split(",")))
+
+
+def count_fish(initial_state, days):
+    copy_state = initial_state
+    for day in range(days):
+        tmp_count = defaultdict(int)
+        for i, j in copy_state.items():
+            if (i == 0):
+                tmp_count[6] += j
+                tmp_count[8] += j
+            else:
+                tmp_count[i - 1] += j
+        copy_state = tmp_count
+    return sum(copy_state.values())
+
+
+print(count_fish(data, 80))
+print(count_fish(data, 256))
