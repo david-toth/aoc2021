81bc6e1cddc7895156ba8b95df55cc79f1a33e36
David Toth <davidtoth99@gmail.com>
2021-12-07 09:22:14 -0500

    Update main.py

--- d07/main.py
+++ d07/main.py
@@ -6,23 +6,17 @@ data = np.array(list(map(int, lines[0].strip().split(","))))
 file.close()
 
 
-def absolute_distance(x_array, target):
-    return np.sum(np.abs(x_array - target))
-
-
 def modified_fuel_intake(x):
     return 0.5 * x * (x + 1)
 
 
 # Part one solution 
 median = np.median(data)
-print(absolute_distance(data, median))
+print(np.sum(np.abs(data - median)))
 
 # Part two solution
-min_fuel = int(1e9)
-for i in range(data.max() + 1):
-    distance = np.abs(data - i)
-    weighted_dist = np.sum(modified_fuel_intake(distance))
-    if weighted_dist < min_fuel:
-        min_fuel = weighted_dist
-print(min_fuel)
+optimal_floor, optimal_ceil = np.floor(data.mean()), np.ceil(data.mean())
+dist_floor, dist_ceil = np.abs(data - optimal_floor), np.abs(data - optimal_ceil)
+weighted_floor, weighted_ceil = (np.sum(modified_fuel_intake(dist_floor)),
+                                 np.sum(modified_fuel_intake(dist_ceil)))
+print(np.min([weighted_floor, weighted_ceil]))
