e5f5a92145fd4f5461b1ef8e0daa341b1067535c
David Toth <davidtoth99@gmail.com>
2021-12-07 08:57:51 -0500

    d07 solution

---
+++ d07/main.py
@@ -0,0 +1,32 @@
+import numpy as np
+
+file = open("input.txt", "r")
+lines = file.readlines()
+data = np.array(list(map(int, lines[0].strip().split(","))))
+file.close()
+
+
+def absolute_distance(x_array, target):
+    return np.sum(np.abs(x_array - target))
+
+
+def modified_fuel_intake(x):
+    return 0.5 * x * (x + 1)
+
+
+# Part one solution 
+min_fuel = int(1e9)
+for i in range(data.max() + 1):
+    distance = absolute_distance(data, i)
+    if distance < min_fuel:
+        min_fuel = distance
+print(min_fuel)
+
+# Part two solution
+min_fuel = int(1e9)
+for i in range(data.max() + 1):
+    distance = np.abs(data - i)
+    weighted_dist = np.sum(modified_fuel_intake(distance))
+    if weighted_dist < min_fuel:
+        min_fuel = weighted_dist
+print(min_fuel)
