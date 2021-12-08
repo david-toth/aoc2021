18e3a0bc81fb4521719d63e04775857c44b20a06
David Toth <davidtoth99@gmail.com>
2021-12-08 15:52:01 -0500

    d08 solution

---
+++ d08/main.py
@@ -0,0 +1,28 @@
+import itertools
+
+with open("input.txt", "r") as file:
+    lines = file.readlines()
+
+# Part 1 - EZ
+count = 0
+for line in lines:
+    start, end = line.split(" | ")
+    for s in end.split():
+        if (len(s) in [2, 3, 4, 7]):
+            count += 1
+print(count)
+
+# Part 2 - RIP
+count = 0
+orig_signals = ["abcefg", "cf", "acdeg", "acdfg", "bcdf", "abdfg", "abdefg", "acf", "abcdefg", "abcdfg"]
+orig_set = set(orig_signals)
+for line in lines:
+    start, end = line.split(" | ")
+    for p in itertools.permutations("abcdefg"):
+        x = {i: j for i, j in zip(p, "abcdefg")}
+        y = set("".join(sorted(map(x.get, s))) for s in start.split())
+        if y == orig_set:
+            ouputs = ["".join(sorted(map(x.get, s))) for s in end.split()]
+            outputs = "".join(str(orig_signals(s)) for s in b)
+            count += int(outputs)
+print(count)
